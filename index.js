d28ba474af706d4d71cfdd90f77a6ae2b253fad1
soy buena onda <alirio.fernandez24@hotmail.com>
2023-03-24 16:56:38 -0400

    funciona

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
+const { body, validationResult } = require('express-validator');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Configuración del puerto
+const port = process.env.PORT || 5000;
+
+// Configuración de la base de datos
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+dialect: 'mysql'
+});
+
+
+// Definición del modelo de la base de datos
+const Usuarios = sequelize.define('usuarios', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre_apellido: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    validate: {
+      isEmail: true
+    },
+    indexes: [{ unique: true, fields: ["email"] }]
+  },
+  zona_residencial: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  telefono: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  }
+});
+
+app.use(express.json());
+
+// Definición de las rutas de la API
+app.post('/usuarios', [
+  body('nombre_apellido').notEmpty().withMessage('El campo nombre_apellido es requerido'),
+  body('email').notEmpty().withMessage('El campo email es requerido'),
+  body('email').isEmail().withMessage('El campo email debe ser una dirección de correo electrónico válida'),
+  body('zona_residencial').notEmpty().withMessage('El campo zona_residencial es requerido'),
+  body('telefono').notEmpty().withMessage('El campo telefono es requerido')
+], async (req, res) => {
+
+  // Validación de entrada
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Validación de datos duplicados en el backend
+  const { nombre_apellido, email, zona_residencial, telefono } = req.body;
+  const usuarioExistente = await Usuarios.findOne({ where: { email } });
+  if (usuarioExistente) {
+    return res.status(409).json({ message: 'El correo electrónico ya existe en la base de datos' });
+  }
+
+  // Guardando en la base de datos
+  const nuevoUsuario = await Usuarios.create({
+    nombre_apellido,
+    email,
+    zona_residencial,
+    telefono
+  });
+  return res.json(nuevoUsuario);
+});
+
+app.get('/usuarios', async (req, res) => {
+  const page = req.query.page || 1;
+  const per_page = req.query.per_page || 10;
+  const offset = (page - 1) * per_page;
+  const limit = per_page;
+  const { count, rows: usuarios } = await Usuarios.findAndCountAll({
+    offset,
+    limit
+  });
+  return res.json({
+    data: usuarios,
+    total: count,
+    pages: Math.ceil(count / limit)
+  });
+});
+
+// Sincronización de la base de datos y ejecución de la aplicación de Express
+sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto", port);
+  });
+});
